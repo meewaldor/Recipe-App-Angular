d69d6a3ffc2754db18b8d509cdf8eb64cd531465
meewaldor <tieumylam91@gmail.com>
2024-04-23 08:44:15 +0700

    Making Http request

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,28 +4,35 @@ import { Subject } from "rxjs";
 
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>;
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe',
-        'This is simply a test', 
-        "https://hips.hearstapps.com/hmg-prod/images/crepes-lead-64347419487e4.jpg?crop=0.9995238095238095xw:1xh;center,top&resize=768:*",
-        [
-            new Ingredient('Meat',1),
-            new Ingredient('French Fries',20)
-        ]),
-        new Recipe('Another Test Recipe',
-        'This is simply a test', 
-        "https://img.freepik.com/premium-psd/italian-food-vegan-spagetti-italian-dish-pasta-food_396469-106.jpg",
-        [
-            new Ingredient('Meat',2),
-            new Ingredient('Buns',10)
-        ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('A Test Recipe',
+    //     'This is simply a test', 
+    //     "https://hips.hearstapps.com/hmg-prod/images/crepes-lead-64347419487e4.jpg?crop=0.9995238095238095xw:1xh;center,top&resize=768:*",
+    //     [
+    //         new Ingredient('Meat',1),
+    //         new Ingredient('French Fries',20)
+    //     ]),
+    //     new Recipe('Another Test Recipe',
+    //     'This is simply a test', 
+    //     "https://img.freepik.com/premium-psd/italian-food-vegan-spagetti-italian-dish-pasta-food_396469-106.jpg",
+    //     [
+    //         new Ingredient('Meat',2),
+    //         new Ingredient('Buns',10)
+    //     ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
     getRecipes() {
         return this.recipes.slice(); //get a copy
     }
     getRecipe(index:number) {
         return this.recipes[index];
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     addRecipe(recipe: Recipe){
